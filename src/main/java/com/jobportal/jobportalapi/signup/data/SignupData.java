bc55f4fbeaa5fe4f5373ec57fb3fec9dc7831371
johnebri <john.ebr@yahoo.com>
2020-03-03 17:57:29 +0100

    signup data

--- src/main/java/com/jobportal/jobportalapi/signup/data/SignupData.java
+++ src/main/java/com/jobportal/jobportalapi/signup/data/SignupData.java
@@ -1,5 +1,32 @@
 package com.jobportal.jobportalapi.signup.data;
 
 public class SignupData {
+	
+	private String username;
+	private String password;
+	private String email;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
 
 }
